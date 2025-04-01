c2e3bbcb0057af1c3da4d2176b33be8f3469d634
unknown <noman40046@gmail.com>
2025-04-01 10:59:51 +0600

    why us

--- src/components/WhyUsSection.jsx
+++ src/components/WhyUsSection.jsx
@@ -1,103 +1,45 @@
 
+import whyUsData from "../../public/data/whychoose.json";
+
 const WhyUsSection = () => {
-    return (
-        <div>
-            <section className="py-10 bg-white sm:py-16 lg:py-24">
-                <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-                    <div className="max-w-2xl mx-auto text-center">
-                        <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">বৃত্ত লার্নিং কেনো ?</h2>
-                        <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-600">আমাদের কোর্স সবার থেকে আলাদা কেনো সেটি বুঝতে হলে আমাদের ফ্রি কোর্সগুলোই যথেষ্ট</p>
+  return (
+    <div>
+      <section className="py-10 bg-white sm:py-16 lg:py-24">
+        <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+          <div className="max-w-2xl mx-auto text-center">
+            <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
+              বৃত্ত লার্নিং কেনো ?
+            </h2>
+            <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-600">
+              আমাদের কোর্স সবার থেকে আলাদা কেনো সেটি বুঝতে হলে আমাদের ফ্রি কোর্সগুলোই যথেষ্ট
+            </p>
+          </div>
+
+          <div className="grid grid-cols-1 gap-6 mt-12 lg:mt-16 xl:gap-10 sm:grid-cols-2 lg:grid-cols-3">
+            {whyUsData.map((item) => (
+              <div key={item.id} className="overflow-hidden bg-white rounded shadow">
+                <div className="p-8">
+                  <div className="flex flex-col justify-center items-center">
+                    <img
+                      className="flex-shrink-0 w-20 h-auto mb-5"
+                      src={item.image}
+                      alt={item.title}
+                    />
+                    <div>
+                      <p className="text-3xl font-semibold text-black">{item.title}</p>
                     </div>
-
-                    <div className="grid grid-cols-1 gap-6 mt-12 lg:mt-16 xl:gap-10 sm:grid-cols-2 lg:grid-cols-3">
-                        <div className="overflow-hidden bg-white rounded shadow">
-                            <div className="p-8">
-                                <div className="flex flex-col justify-center items-center">
-                                    <img className="flex-shrink-0 w-20 h-auto mb-5" src="/public/img/whyus/online-learning.png" alt="" />
-                                    <div className="">
-                                        <p className="text-3xl font-semibold text-black">অভিজ্ঞ ও পেশাদার মেন্টর
-                                        </p>
-                                    </div>
-                                    <p className=" text-xl leading-relaxed  text-center text-black mt-7">মার্কেটের অভিজ্ঞ ও প্রফেশনাল মেন্টর কর্তৃক প্রতিটি কোর্স তৈরি করা হয়েছে।</p>
-                                </div>
-                                
-                            </div>
-                        </div>
-
-                        <div className="overflow-hidden bg-white rounded shadow">
-                            <div className="p-8">
-                                <div className="flex flex-col justify-center items-center">
-                                    <img className="flex-shrink-0 w-20 h-auto mb-5" src="/public/img/whyus/self-employed.png" alt="" />
-                                    <div className="">
-                                        <p className="text-3xl  font-semibold text-black">স্কিল ডেভেলপমেন্ট
-                                        </p>
-                                    </div>
-                                    <p className=" text-xl leading-relaxed text-center text-black mt-7">মার্কেটের অভিজ্ঞ ও প্রফেশনাল মেন্টর কর্তৃক প্রতিটি কোর্স তৈরি করা হয়েছে।</p>
-                                </div>
-                                
-                            </div>
-                        </div>
-
-                        <div className="overflow-hidden bg-white rounded shadow">
-                            <div className="p-8">
-                                <div className="flex flex-col justify-center items-center">
-                                    <img className="flex-shrink-0 w-20 h-auto mb-5" src="/public/img/whyus/archive.png" alt="" />
-                                    <div className="">
-                                        <p className="text-3xl font-semibold text-black">প্রয়োজনীয় রিসোর্স সংগ্রহ</p>
-                                    </div>
-                                    <p className=" text-xl leading-relaxed text-center text-black mt-7">মার্কেটের অভিজ্ঞ ও প্রফেশনাল মেন্টর কর্তৃক প্রতিটি কোর্স তৈরি করা হয়েছে।</p>
-                                </div>
-                               
-                            </div>
-                        </div>
-
-                        <div className="overflow-hidden bg-white rounded shadow">
-                            <div className="p-8">
-                                <div className="flex flex-col justify-center items-center">
-                                    <img className="flex-shrink-0 w-20 h-auto mb-5" src="/public/img/whyus/document.png" alt="" />
-                                    <div className="">
-                                        <p className="text-3xl font-semibold text-black">সার্টিফিকেশন</p>
-                                    </div>
-                                    <p className=" text-xl leading-relaxed text-center text-black mt-7">মার্কেটের অভিজ্ঞ ও প্রফেশনাল মেন্টর কর্তৃক প্রতিটি কোর্স তৈরি করা হয়েছে।</p>
-                                </div>
-                              
-                            </div>
-                        </div>
-
-                        <div className="overflow-hidden bg-white rounded shadow">
-                            <div className="p-8">
-                                <div className="flex flex-col justify-center items-center">
-                                    <img className="flex-shrink-0 w-20 h-auto mb-5" src="/public/img/whyus/software.png" alt="" />
-                                    <div className="">
-                                        <p className="text-3xl font-semibold text-black">ক্যারিয়ার সাপোর্ট</p>
-                                    </div>
-                                    <p className=" text-xl leading-relaxed text-center text-black mt-7">মার্কেটের অভিজ্ঞ ও প্রফেশনাল মেন্টর কর্তৃক প্রতিটি কোর্স তৈরি করা হয়েছে।</p>
-                                </div>
-                               
-                            </div>
-                        </div>
-
-                        <div className="overflow-hidden bg-white rounded shadow">
-                            <div className="p-8">
-                                <div className="flex flex-col justify-center items-center">
-                                    <img className="flex-shrink-0 w-20 h-auto mb-5" src="/public/img/whyus/sync.png" alt="" />
-                                    <div className="">
-                                        <p className="text-3xl font-semibold text-black">আপডেটেড কোর্স মডিউল</p>
-                                    </div>
-                                    <p className=" text-xl leading-relaxed text-center text-black mt-7">মার্কেটের অভিজ্ঞ ও প্রফেশনাল মেন্টর কর্তৃক প্রতিটি কোর্স তৈরি করা হয়েছে।</p>
-                                </div>
-                              
-                            </div>
-                        </div>
-
-                       
-                    </div>
-
+                    <p className="text-xl leading-relaxed text-center text-black mt-7">
+                      {item.description}
+                    </p>
+                  </div>
                 </div>
-            </section>
-
+              </div>
+            ))}
+          </div>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default WhyUsSection;
