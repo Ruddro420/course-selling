49f078c0ef0002279e84e71cf0397d90335b3d12
touhidul-islam-html <ti360109@gmail.com>
2025-04-06 12:43:11 +0600

    update

--- src/components/courseComponent/CourseHero.jsx
+++ src/components/courseComponent/CourseHero.jsx
@@ -125,10 +125,10 @@ const CourseHero = ({ course, sloading }) => {
       {
         sloading ? <Loader /> : <div className="">
           <div className="course-container py-80 course-hero-section  h-80 flex items-center justify-between mx-auto p-4 lg:py-56">
-            <div className="container mx-auto lg:mt-10 mt-42 max-w-screen-xl md:px-12">
-              <div className="flex flex-col course-hero md:flex-row md:gap-12">
+            <div className="container mx-auto lg:mt-10 mt-42 md:mt-10 max-w-screen-xl md:px-4 lg:px-12">
+              <div className="flex flex-col course-hero md:flex-row md:gap-5 lg:gap-12">
                 <div className="md:w-1/2 lg:w-2/3 pt-20 px-6 flex-grow hero-cards">
-                  <h1 className=" md:text-4xl text-3xl text-white font-bold mb-6">
+                  <h1 className=" md:text-4xl lg:mt-0 md:mt-0 mt-20 text-3xl text-white font-bold mb-6">
                     {course.course_name}
                   </h1>
                   <p className="text-md md:text-xl lg:text-xl text-gray-400 mb-8">
